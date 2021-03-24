6ff5c863c34b9d1f9d1425e1fbc912151c7cb534
Walter <walterrubicondo@gmail.com>
2021-03-24 17:25:38 +0100

    Fine Esercizio

---
+++ main.js
@@ -0,0 +1,8 @@
+var app = new Vue ({
+
+  el: "#root",
+  data: {
+    messaggio: "Sciao belo",
+    immagine: "https://www.laputa.it/wp-content/uploads/2020/02/Rasta_rules_485080279.jpg",
+  },
+})
